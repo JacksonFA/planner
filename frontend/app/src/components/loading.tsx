aa603731fa6bfe2f105e8980455717293efe1951
JacksonFA <jackson.arceno@seventh.com.br>
2024-07-12 21:21:59 -0300

    app: initial commit

---
+++ frontend/app/src/components/loading.tsx
@@ -0,0 +1,5 @@
+import { ActivityIndicator, ActivityIndicatorBase } from 'react-native'
+
+export function Loading() {
+  return <ActivityIndicator className='flex-1 bg-zinc-950 items-center justify-center text-lime-300' />
+}
---
+++ frontend/app/src/server/trip-server.ts
@@ -0,0 +1,66 @@
+import { api } from "./api"
+
+export type TripDetails = {
+  id: string
+  destination: string
+  starts_at: string
+  ends_at: string
+  is_confirmed: boolean
+}
+
+type TripCreate = Omit<TripDetails, "id" | "is_confirmed"> & {
+  emails_to_invite: string[]
+}
+
+async function getById(id: string) {
+  try {
+    const { data } = await api.get<{ trip: TripDetails }>(`/trips/${id}`)
+    return data.trip
+  } catch (error) {
+    throw error
+  }
+}
+
+async function create({
+  destination,
+  starts_at,
+  ends_at,
+  emails_to_invite,
+}: TripCreate) {
+  try {
+    console.log("Creating a new trip", api.getUri())
+    const { data } = await api.post<{ tripId: string }>("/trips", {
+      destination,
+      starts_at,
+      ends_at,
+      emails_to_invite,
+      owner_name: "Jackson Arceno",
+      owner_email: "jackson.f205@gmail.com",
+    })
+
+    return data
+  } catch (error: any) {
+
+    console.log(error.message)
+    throw error
+  }
+}
+
+async function update({
+  id,
+  destination,
+  starts_at,
+  ends_at,
+}: Omit<TripDetails, "is_confirmed">) {
+  try {
+    await api.put(`/trips/${id}`, {
+      destination,
+      starts_at,
+      ends_at,
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+export const tripServer = { getById, create, update }
